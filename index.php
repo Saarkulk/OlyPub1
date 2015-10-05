fad6bf5f8d79d06469997eb6e2266aca09156768
Saarkulk <bedeister@gmail.com>
2015-10-05 16:02:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+
+$line = date('Y-m-d H:i:s');
+
+$ipAddress = $_SERVER['REMOTE_ADDR'];
+
+if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+    $ipAddress = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
+}
+
+if ((strcmp($ipAddress, '10.4.32.59') != 0) and (strcmp($ipAddress, '10.4.192.228') != 0) ) {
+	
+	$line .= " - " . $ipAddress . " - " . $_SERVER['HTTP_USER_AGENT'] . " -- " . $_SERVER['REMOTE_PORT'];
+	file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
+}
+
+?>
 <!DOCTYPE html>
 <!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
 <!--[if !IE]><!-->
